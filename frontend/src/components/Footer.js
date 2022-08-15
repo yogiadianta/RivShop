641d243c3fffc6e554141fee4f5f617ea91ed81e
Yogi Adianta <yogiadianta0@gmail.com>
2022-08-15 15:58:27 +0700

    Add Header, Footer component and React Bootstrap

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react' 
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+    return (
+        <footer>
+            <Container>
+                <Col className='text-center py-3'>
+                    Copyright &Copy; RivShop
+                </Col>
+            </Container>
+        </footer>
+    )
+}
+export default Footer
