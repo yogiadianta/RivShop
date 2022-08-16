2e4a6aadfd52e2179ad64ca5a55a1fd23f3dbfc1
Yogi Adianta <yogiadianta0@gmail.com>
2022-08-16 14:12:56 +0700

    Add rating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,17 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
-  return (
-    <>
-      <Header />
-      <main className='py-3'>
-          <Container>
-            <HomeScreen />
-          </Container>
-      </main>
-      <Footer />
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <main className='py-3'>
+                <Container>
+                    <HomeScreen />
+                </Container>
+            </main>
+            <Footer />
+        </>
+    )
 }
 
 export default App
