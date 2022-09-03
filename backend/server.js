f2b9d061e3c99ee03de337167862d5a52f064158
Yogi Adianta <yogiadianta0@gmail.com>
2022-09-04 03:31:44 +0700

    Connecting to Mongo Database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import color from 'color'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+connectDB()
+
 const app = express()
 
 app.get('/', (req, res) => {
