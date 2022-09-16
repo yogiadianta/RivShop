511012664e34981a57581994b8310f11d98c3d67
Yogi Adianta <yogiadianta0@gmail.com>
2022-09-16 17:43:40 +0700

    Product details, reducer, and action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,4 +1,11 @@
-import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL} from '../constants/productConstant'
+import {
+    PRODUCT_LIST_REQUEST, 
+    PRODUCT_LIST_SUCCESS, 
+    PRODUCT_LIST_FAIL, 
+    PRODUCT_DETAILS_SUCCESS, 
+    PRODUCT_DETAILS_FAIL, 
+    PRODUCT_DETAILS_REQUEST
+} from '../constants/productConstant'
 import axios from 'axios'
 
 export const listProducts = () => async (dispatch) => {
@@ -14,6 +21,24 @@ export const listProducts = () => async (dispatch) => {
     }catch(error){
         dispatch({
             type: PRODUCT_LIST_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+export const listProductDetails = (id) => async (dispatch) => {
+    try{
+        dispatch({ type: PRODUCT_DETAILS_REQUEST})
+
+        const {data} = await axios.get(`/api/products/${id}`)
+
+        dispatch({
+            type:  PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        })
+    }catch(error){
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message : error.message
         })
     }
